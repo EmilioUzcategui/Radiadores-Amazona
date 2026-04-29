e378acc3d590a2b3b8220dafa4d8d744afcf8f8f
Cristian Guevara <cristianguevara0331@gmail.com>
2026-04-29 19:14:04 -0400

    Conexión con base de datos de SupaBase lista

--- backend/src/config/database/db.ts
+++ backend/src/config/database/db.ts
@@ -11,6 +11,8 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT || '5432'),
+    // Supabase requiere SSL para conexiones remotas
+    ssl: { rejectUnauthorized: false },
 
     // Opciones recomendadas para producción
     max: 20, // Número máximo de clientes en el pool
